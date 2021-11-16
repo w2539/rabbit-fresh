a20490e7b2913329e1272523b0f8645cd6893228
wlb <2539550474@qq.com>
2021-11-16 23:58:08 +0800

    11月16日第二次提交-批量注册全局组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -4,21 +4,41 @@
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
 import defaultImg from '@/assets/images/200.png'
 
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from '@/components/library/xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from '@/components/library/xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
+
+// 使用 require 提供的函数 context 加载某一个目录下的所有 .vue 后缀的文件。
+// 然后 context 函数会返回一个导入函数 importFn
+// 它又一个属性 keys() 获取所有的文件路径
+// 通过文件路径数组，通过遍历数组，再使用 importFn 根据路径导入组件对象
+// 遍历的同时进行全局注册即可
+
+// 导入library文件夹下的所有组件
+// 批量导入需要使用一个函数 require.context(dir,deep,matching)
+// 参数：1. 目录  2. 是否加载子目录  3. 加载的正则匹配
+const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install (app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
+
+    importFn.keys().forEach((key) => {
+      // 导入组件
+      const component = importFn(key).default
+      // 注册组件
+      
+      app.component(component.name, component)
+    })
 
     // 定义指令
     defineDirective(app)
